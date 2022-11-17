2c488f9ac4d8075c4f6153e9ccedd4e0b200960c
Mr-Code-Secure <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-17 11:53:37 +0530

    Program Five

---
+++ Addition Program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Enter Frist Value");
+    scanf("%d",&a);
+    printf("Enter Secnd Value");
+    scanf("%d",&b);
+    sum = a+b;
+    printf("Add is = %d",sum);
+    return 0;
+}

17994f3e514649f101c55fc25a5e8720beffcf10
Mr-Code-Secure <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-17 11:19:32 +0530

    Second Program

--- Integer Value Printer.c
+++ Integer Value Printer.c
@@ -3,10 +3,11 @@
 //      //  ->  Single Line Comment
 //      /*  ->  Multiline Comment
 
-int main (void)
+int main()
 {
     int a;  // a is a variable of type int which contain some data
     printf("Enter A Number");
     scanf("%d",&a);     // & -> address of operator
     printf("A is = %d",a);
+    return 0;
 }

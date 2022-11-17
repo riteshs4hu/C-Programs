66fe8bfcc03aabb88debe5f3cc671d60ca22d939
Mr-Code-Secure <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-17 11:16:22 +0530

    Third Program

--- Decimal Value Printer.c
+++ Decimal Value Printer.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
-int main (void)
+int main()
 {
     float a;  // a is a variable of type int which contain some data
     printf("Enter A Number");
     scanf("%f",&a);     // & -> address of operator 
     printf("A is = %f",a);
+    return 0;
 }

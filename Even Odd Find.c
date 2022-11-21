eb0d707cb98906d953373c0239d6d5212a52714c
Mr-Code-Secure <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-21 11:02:38 +0530

    Program No. 12

---
+++ Even Odd Find.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a Value\n");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("The Number is Even");
+    }
+    else
+    {
+        printf("The Number is Odd");
+    }
+}

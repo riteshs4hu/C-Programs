08e9abd04e022986fbf87570350cc1f82f1e30ff
Mr-Code-Secure <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-21 10:30:22 +0530

    Tenth Program
    Easy If Statement Code

---
+++ esle Statment.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter A And B\n");
+    scanf("%d%d",&a,&b);
+    if (a>b)
+    {
+        printf("a is Greater");
+    }
+    else
+    {
+        printf("B is Greater");
+    }
+}

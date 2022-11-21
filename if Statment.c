7ab7fe0f0b9722f187c4b6c60ac099cf5f3a914a
Mr-Code-Secure <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-21 10:14:24 +0530

    Ninght Program
    Conditional Statment

---
+++ if Statment.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter First Value\n");
+    scanf("%d",&a);
+    printf("Enter Second Value\n");
+    scanf("%d",&b);
+    if (a>b)
+    {
+        printf("A is Greater");
+    }
+    if (b>a);
+    {
+        printf("B is Greater");
+    }
+    return 0;
+}

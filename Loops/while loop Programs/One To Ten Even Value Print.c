a1c0a1c9463539af0675c8007b461743a747e04c
Ritesh Sahu <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-24 12:15:28 +0530

    while 3

---
+++ Loops/while loop Programs/One To Ten Even Value Print.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        if (i%2==0)
+        {
+        printf("%d\n",i);
+        }
+        i++;
+    }
+    return 0;
+}

f1df7872e93cb989ce82a59fe0cb4f232f4761a1
Ritesh Sahu <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-26 13:32:38 +0530

    do while 4

---
+++ Loops/do while loop/One To Ten Odd Value.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=1;
+    do
+    {
+        if (i%2!=0)
+        {
+        printf("%d\n",i);
+        }
+        i++;
+    }
+    while(i<=10);
+    return 0;
+}

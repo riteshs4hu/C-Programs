862c49833d75d6614d5b4f125b8e7757903c0e55
Ritesh Sahu <118372301+Mr-Secure-Code@users.noreply.github.com>
2022-11-26 13:43:41 +0530

    while 5

---
+++ Loops/while loop Programs/Revers Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,rev=0,r;
+    printf("Enter A Value\n");
+    scanf("%d",&n); 
+    while(n>0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+
+    printf("Rev Number is = %d\n", rev);
+}
